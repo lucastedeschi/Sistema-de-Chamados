b54f90ab0943fd53af06c4820be896dbcce7c5cc
VictorMessina <lucas@DESKTOP-4TCA580>
2016-04-22 19:03:24 -0300

    Atualização da classe de teste da entidade RegistroChamado

--- test/entidade/RegistroChamadoTest.java
+++ test/entidade/RegistroChamadoTest.java
@@ -5,10 +5,23 @@
  */
 package entidade;
 
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
 /**
  *
  * @author lucas
  */
 public class RegistroChamadoTest {
-    
+    @Test
+    public void inicializacaoTest(){
+      
+        Tecnico t = new Tecnico("Tecnico", 123456);
+        Empresa empresa = new Empresa(123456789, "Empresa");
+        ClienteEmpresa cq = new ClienteEmpresa(1, empresa, 123456, "ClienteEmpresa", 12345678);
+        Chamado c = new Chamado("Titulo", "Descrição", 1, t, cq, "Windows", "10", "BD");
+        RegistroChamado r = new RegistroChamado("Assunto", c, t);
+        assertEquals("Assunto", r.getAssunto());
+        assertEquals(t, r.getTecnico());
+    }
 }
