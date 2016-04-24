a2b877ea3f8ada0613e58aefc355ac690a38b3ff
lucas <lucas@DESKTOP-4TCA580>
2016-04-24 10:13:43 -0300

    Teste de alteração da classe de teste da entidade RegistroChamado

--- test/entidade/RegistroChamadoTest.java
+++ test/entidade/RegistroChamadoTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
 public class RegistroChamadoTest {
     @Test
     public void inicializacaoTest(){
-      
         Tecnico t = new Tecnico("Tecnico", 123456);
         Empresa empresa = new Empresa(123456789, "Empresa");
         ClienteEmpresa cq = new ClienteEmpresa(1, empresa, 123456, "ClienteEmpresa", 12345678);
@@ -23,5 +22,21 @@ public class RegistroChamadoTest {
         RegistroChamado r = new RegistroChamado("Assunto", c, t);
         assertEquals("Assunto", r.getAssunto()); 
         assertEquals(t, r.getTecnico()); 
+    }
+    
+    @Test
+    public void alteracaoTest(){
+        Tecnico t = new Tecnico("Tecnico", 123456);
+        Empresa empresa = new Empresa(123456789, "Empresa");
+        ClienteEmpresa cq = new ClienteEmpresa(1, empresa, 123456, "ClienteEmpresa", 12345678);
+        Chamado c = new Chamado("Titulo", "Descrição", 1, t, cq, "Windows", "10", "BD");
+        RegistroChamado r = new RegistroChamado("Assunto", c, t);
+        assertEquals("Assunto", r.getAssunto()); 
+        assertEquals(t, r.getTecnico()); 
+        Tecnico t2 = new Tecnico("Tecnico2", 123456);
+        r.setAssunto("Assunto2");
+        r.setTecnico(t2);
+        assertEquals("Assunto2", r.getAssunto());
+        assertEquals(t2, r.getTecnico());
     }
 }
