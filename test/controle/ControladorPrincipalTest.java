f35ecd6f6e5480f173f15085348c68720974baca
lucas <lucas@DESKTOP-4TCA580>
2016-04-26 19:18:54 -0300

    Criação da classe de teste da classe de controle ControladorPrincipal

---
+++ test/controle/ControladorPrincipalTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+import static junit.framework.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ *
+ * @author lucas
+ */
+public class ControladorPrincipalTest {
+
+    @Test
+    public void incializacaoTest() {
+        ControladorPrincipal c = new ControladorPrincipal();
+        assertNotNull(c.getCtrChamados());
+        assertNotNull(c.getCtrClientes());
+        assertNotNull(c.getCtrTecnicos());
+        assertNotNull(c.getCtrEmpresa());
+    }
+
+}
