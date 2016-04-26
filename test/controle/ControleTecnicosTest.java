85c8be98344b49b62daa2753cb5dff6713bf3115
lucas <lucas@DESKTOP-4TCA580>
2016-04-26 19:22:15 -0300

    Criação da classe de teste da classe de controle ControleTecnicos

---
+++ test/controle/ControleTecnicosTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+import static junit.framework.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author lucas
+ */
+public class ControleTecnicosTest {
+    @Test
+    public void inserirTest() {
+        //Método não encontrado
+    }
+
+    @Test
+    public void updateTest() {
+        //Método não encontrado
+    }
+
+    @Test
+    public void removeTest() {
+        //Método não encontrado
+    }
+}
