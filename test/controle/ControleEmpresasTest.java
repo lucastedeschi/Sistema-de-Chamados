5d28438882a55e966560fe17869d091116a5d28a
lucas <lucas@DESKTOP-4TCA580>
2016-04-26 21:22:46 -0300

    Criação da classe de teste da classe de controle ControleEmpresas

---
+++ test/controle/ControleEmpresasTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+import entidade.Empresa;
+import junit.framework.Assert;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author lucas
+ */
+public class ControleEmpresasTest {
+    
+    @Test
+    public void inserirEmpresaTest() {
+        ControleEmpresas ce = new ControleEmpresas();
+        Empresa e = new Empresa(12345670, "Empresa5");
+        ce.inserir(e.getNumeroContrato(), e.getNomeEmpresa());
+        Empresa ei = ce.retorna(12345670, "Empresa5");
+        assertEquals(ei.getNomeEmpresa(), e.getNomeEmpresa());
+        assertEquals(ei.getNumeroContrato(), e.getNumeroContrato());
+    }
+
+    @Test
+    public void readTest() {
+        Empresa e = new Empresa(123456789, "Empresa8");
+        ControleEmpresas c = new ControleEmpresas();
+        c.inserir(e.getNumeroContrato(), e.getNomeEmpresa());
+        Empresa ei = c.retorna(123456789, "Empresa8");
+        assertEquals("Empresa8", ei.getNomeEmpresa());
+        assertEquals(123456789, ei.getNumeroContrato());
+        
+    }
+
+     @Test
+    public void removeTest() {
+        //Método não encontrado
+    }
+    
+    @Test
+    public void updateTest() {
+        //Método não encontrado
+    }
+}
