ad84c9c21bce28ee820414c8772c85c451827e70
lucas <lucas@DESKTOP-4TCA580>
2016-04-26 21:32:10 -0300

    Atualização da classe de teste ControleEmpresaTest

--- test/controle/ControleEmpresasTest.java
+++ test/controle/ControleEmpresasTest.java
@@ -9,6 +9,7 @@ import entidade.Empresa;
 import junit.framework.Assert;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -16,15 +17,23 @@ import org.junit.Test;
  * @author lucas
  */
 public class ControleEmpresasTest {
-    
+
     @Test
     public void inserirEmpresaTest() {
         ControleEmpresas ce = new ControleEmpresas();
         Empresa e = new Empresa(12345670, "Empresa5");
         ce.inserir(e.getNumeroContrato(), e.getNomeEmpresa());
         Empresa ei = ce.retorna(12345670, "Empresa5");
-        assertEquals(ei.getNomeEmpresa(), e.getNomeEmpresa());
-        assertEquals(ei.getNumeroContrato(), e.getNumeroContrato());
+        assertTrue(ei.getNomeEmpresa().equals(e.getNomeEmpresa()));
+        assertTrue(ei.getNumeroContrato() == e.getNumeroContrato());
+    }
+
+    @Test
+    public void inserirEmpresaDuplicadaTest() {
+        ControleEmpresas c = new ControleEmpresas();
+        Empresa ee = c.retorna(12345670, "Empresa5");
+        assertNull(c.inserir(ee.getNumeroContrato(), ee.getNomeEmpresa()));
+
     }
 
     @Test
@@ -35,22 +44,14 @@ public class ControleEmpresasTest {
         Empresa ei = c.retorna(123456789, "Empresa8");
         assertEquals("Empresa8", ei.getNomeEmpresa());
         assertEquals(123456789, ei.getNumeroContrato());
-        
-    }
-    
-     @Test
-    public void inserirEmpresaDuplicadaTest() {
-        ControleEmpresas c = new ControleEmpresas();
-        Empresa e = c.retorna(12345670, "Empresa5");
-        assertNull(c.inserir(e.getNumeroContrato(), e.getNomeEmpresa()));
 
     }
 
-     @Test
+    @Test
     public void removeTest() {
         //Método não encontrado
     }
-    
+
     @Test
     public void updateTest() {
         //Método não encontrado
