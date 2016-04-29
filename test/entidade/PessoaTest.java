db23c1cc1185efc593dad7af737379f86c56f07e
VictorMessina <>
2016-04-29 14:36:17 -0300

    Incremento de mais cenários de teste na classe Pessoa

--- test/entidade/PessoaTest.java
+++ test/entidade/PessoaTest.java
@@ -31,5 +31,23 @@ public class PessoaTest {
         Assert.assertEquals(55555,p1.getTelefone());
         Assert.assertEquals("João",p1.getNome());
     }
+    
+    @Test
+    public void inicializacaoNulaTest() {
+        Pessoa p1 = new Pessoa("",4444444);
+        Assert.assertEquals(4444444,p1.getTelefone());
+        Assert.assertNotEquals("",p1.getNome());
+    }
+    
+    @Test
+    public void alteracaoNulaTest(){
+        Pessoa p1 = new Pessoa("José",4444444);
+        Assert.assertEquals(4444444,p1.getTelefone());
+        Assert.assertEquals("José",p1.getNome());
+        p1.setNome("");
+        p1.setTelefone(55555);
+        Assert.assertEquals(55555,p1.getTelefone());
+        Assert.assertNotEquals("",p1.getNome());
+    }
 
 }
